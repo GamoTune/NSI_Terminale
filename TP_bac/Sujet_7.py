6760ec0cacd18ad87bbbcd5571a6ce1ad741703d
GamoTune <arthur.labregere@gmail.com>
2023-12-22 13:14:20 +0100

    Create Sujet_7.py

---
+++ TP_bac/Sujet_7.py
@@ -0,0 +1,28 @@
+#Exercice 1
+
+def fusion(tab1:list, tab2:list):
+    lst = tab1+tab2
+    lst.sort()
+    return lst
+
+
+print(fusion([3, 5], [2, 5]))
+
+
+
+
+romains = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+
+def traduire_romain(nombre) :
+    """ Renvoie l'ecriture decimale du nombre donné en chiffres romains """
+
+    if len(nombre) == 1:
+        return romains[nombre[0]]
+
+    elif romains[nombre[0]] >= romains[nombre[1]]:
+        return romains[nombre[0]] + traduire_romain(nombre[1:])
+    else:
+        return traduire_romain(nombre[1:]) - romains[nombre[0]]
+
+
+print(traduire_romain("CXLII"))
